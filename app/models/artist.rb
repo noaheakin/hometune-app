191b7866307d42176f12222f2d109fa1a8b52ffd
Chanson9892 <chan.hanson5@gmail.com>
2020-09-28 15:18:46 -0700

    added more associations to venue and artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,7 @@
 class Artist < ApplicationRecord
     has_many :artist_comments
     has_many :users, through: :artist_comments
+
+    has_many :concerts
+    has_many :venues, through: :concerts
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ApplicationRecord
     has_many :concerts
     has_many :artists, through: :concerts
+
+    has_many :venue_comments
+    has_many :users, through: :venue_comments
 end
