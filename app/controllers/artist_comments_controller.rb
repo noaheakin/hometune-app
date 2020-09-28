3a192df787a850412b42f9ad17389fd005e934c6
Chanson9892 <chan.hanson5@gmail.com>
2020-09-28 13:26:07 -0700

    added basic crud for artistcommentscontroller

--- app/controllers/artist_comments_controller.rb
+++ app/controllers/artist_comments_controller.rb
@@ -1,2 +1,28 @@
 class ArtistCommentsController < ApplicationController
+    before_action :set_artist_comment, only: [:show]
+
+    def index
+        @artist_comments = ArtistComment.all
+    end
+
+    def show
+    end
+
+    def new
+        @artist_comment = ArtistComment.new
+    end
+
+    def create
+        @artist_comment = ArtistComment.create(artist_comment_params)
+    end
+
+    private
+
+    def artist_comment_params
+        params.require(:artist_comment).permit(:user_id, :artist_id, :content)
+    end
+
+    def set_artist_comment
+        @artist_comment = ArtistComment.find(params[:id])
+    end
 end
