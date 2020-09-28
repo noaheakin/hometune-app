7685c1ac1329b1ffd288b89e4f81270b77cbfd13
Chanson9892 <chan.hanson5@gmail.com>
2020-09-28 15:39:36 -0700

    fixed user resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :concerts, only: [:index, :show]
   resources :venues, only: [:index, :show]
   resources :artists, only: [:index, :show]
-  resources :users, [:show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
