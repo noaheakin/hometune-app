2badfe203bdfd7f2513c72c2597efca28e5db772
noaheakin <noaheakin@gmail.com>
2020-09-30 17:23:31 -0700

    added custom methods to concert model

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -6,5 +6,75 @@ class Concert < ApplicationRecord
     def display_date
         self.date.strftime("%A, %B %d, %Y - %I:%M %p")  
     end
-    #sort by venue, artist, popular artists, popular artists, date both ways, genre, location
+
+    # sorts concerts by venue
+    def self.by_venue
+        self.all.sort_by do |concert| 
+            concert.venue.name
+        end
+    end
+    
+    # sorts concerts by popular artist
+    def self.by_popular_venue
+        self.by_venue.sort_by do |concert|
+            concert.venue.popularity
+        end.reverse
+    end
+
+    # sorts concerts by artist
+    def self.by_artist
+        self.all.sort_by do |concert| 
+            concert.artist.name
+        end
+    end
+
+    # sorts concerts by popular artist
+    def self.by_popular_artist
+        self.by_artist.sort_by do |concert|
+            concert.artist.popularity
+        end.reverse
+    end
+
+    # sorts concerts by closest date
+    def self.by_closest_date
+        self.all.sort_by do |concert|
+            concert.date
+        end
+    end
+
+    # sorts concerts by furthest date
+    def self.by_furthest_date
+        self.all.sort_by do |concert|
+            concert.date
+        end.reverse
+    end
+
+    # shows all 21+ concerts
+    def self.by_twenty_one_and_up
+        self.all.select do |concert|
+            concert.twenty_one_and_up
+        end
+    end
+
+    # shows all under 21 concerts
+    def self.by_under_twenty_one
+        self.all.select do |concert|
+            !concert.twenty_one_and_up
+        end
+    end
+
+    # sorts concerts by location
+    def self.by_location
+        self.by_venue.sort_by do |concert|
+            concert.venue.location
+        end
+    end
+
+    # sorts concerts by genre
+    def self.by_genre
+        self.by_artist.sort_by do |concert|
+            concert.artist.genre
+        end
+    end
+
 end
