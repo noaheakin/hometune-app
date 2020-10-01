9e5201fb01834e94a8990a0f36304f3cb30d4a4e
noaheakin <noaheakin@gmail.com>
2020-10-01 15:20:31 -0700

    built out concert buttons and controller actions

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -9,6 +9,66 @@ class ConcertsController < ApplicationController
     def show
     end
 
+    def by_venue_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_venue
+        render "index"
+    end
+
+    def by_popular_venue_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_popular_venue
+        render "index"
+    end
+
+    def by_artist_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_artist
+        render "index"
+    end
+
+    def by_popular_artist_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_popular_artist
+        render "index"
+    end
+
+    def by_closest_date_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_closest_date
+        render "index"
+    end
+
+    def by_furthest_date_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_furthest_date
+        render "index"
+    end
+
+    def by_twenty_one_and_up_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_twenty_one_and_up
+        render "index"
+    end
+
+    def by_under_twenty_one_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_under_twenty_one
+        render "index"
+    end
+
+    def by_location_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_location
+        render "index"
+    end
+
+    def by_genre_concert_button
+        @concerts = Concert.all
+        @concerts = @concerts.by_genre
+        render "index"
+    end
+
     private
 
     def concert_params
